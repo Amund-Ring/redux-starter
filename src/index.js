8281a3892148488319fb2c871582b813fe4e83e2
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2021-10-05 10:19:51 +0200

    customStore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import store from './customStore';
 import * as actions from './actions';
 
+store.subscribe(() => {
+  console.log('Store changed!');
+});
+
 store.dispatch(actions.bugAdded('New bug'));
 store.dispatch(actions.bugResolved(1));
 
