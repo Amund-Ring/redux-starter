87ee6061cb92d8f6bb90cae3edc60905fb8155bc
Amund-Ring <60844467+Amund-Ring@users.noreply.github.com>
2021-10-12 10:54:41 +0200

    Refactoring

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import store from './store/store';
-import * as actions from './store/actions';
+import configureStore from './store/configureStore';
+// import * as actions from './store/bugs';
+import * as actions from './store/projects';
 import { devToolsEnhancer } from 'redux-devtools-extension';
+
+const store = configureStore();
 
 store.subscribe(() => {
   console.log('Store changed!');
 });
 
-store.dispatch(actions.bugAdded('Bug 1'));
-store.dispatch(actions.bugAdded('Bug 2'));
-store.dispatch(actions.bugAdded('Bug 3'));
-store.dispatch(actions.bugResolved(1));
+store.dispatch(actions.projectAdded({name: 'Project 1'}));
+
+store.dispatch(actions.bugAdded({ description: 'Bug 1' }));
+store.dispatch(actions.bugAdded({ description: 'Bug 2' }));
+store.dispatch(actions.bugAdded({ description: 'Bug 3' }));
+store.dispatch(actions.bugResolved({ id: 1 }));
+
 
 console.log(store.getState());
